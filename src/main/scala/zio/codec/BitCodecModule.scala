4a6a453d3db0ba93d8b193ff4141c9045aa41c09
Dejan Mijic <dmijic@acm.org>
2020-04-19 21:32:18 +0200

    Import commands to local scope

--- src/main/scala/zio/codec/BitCodecModule.scala
+++ src/main/scala/zio/codec/BitCodecModule.scala
@@ -14,6 +14,8 @@ trait BitCodecModule extends CodecModule {
 
   @silent
   private def interpret(input: Chunk[Input], codec: Array[CodecVM]): Either[DecodeError, Any] = {
+    import CodecVM._
+
     val stack: Stack[AnyRef] = Stack()
 
     var i: Int          = 0
@@ -23,28 +25,28 @@ trait BitCodecModule extends CodecModule {
     while (i < codec.length) {
       val instr: CodecVM = codec(i)
       instr match {
-        case CodecVM.Push(value) =>
+        case Push(value) =>
           stack.push(value)
 
-        case CodecVM.Read(min, max) =>
+        case Read(min, max) =>
 //          if (inputIndex + min < input.length
 
-        case CodecVM.CheckSet(s) =>
+        case CheckSet(s) =>
           stack.push(s.contains(stack.pop()).asInstanceOf[AnyRef])
 
-        case CodecVM.JumpEq(ifEqual, otherwise) =>
+        case JumpEq(ifEqual, otherwise) =>
           if (stack.pop().eq(stack.pop())) i = ifEqual else i = otherwise
 
-        case CodecVM.Construct1(f) =>
+        case Construct1(f) =>
           stack.push(f(stack.pop()))
 
-        case CodecVM.Construct2(f) =>
+        case Construct2(f) =>
           stack.push(f(stack.pop(), stack.pop()))
 
-        case CodecVM.Fail(err) =>
+        case Fail(err) =>
           return Left(DecodeError(err, i))
 
-        case CodecVM.StoreRegister0 =>
+        case StoreRegister0 =>
           r0 = stack.pop()
       }
     }
